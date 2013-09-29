8f2b09994ef12e1b6b09e9f445bb862c1cf1a3c2
urban_spawn <urban_spawn@rambler.ru>
2013-09-29 15:12:23 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'cbf7290cd41dcabc523c981f3bb5095b80043eb3f3ff570e69b238f71e29e4b811e1c80f378db3eb19313b58eed155cb894098147197eb50e8ea57311848fce2'
